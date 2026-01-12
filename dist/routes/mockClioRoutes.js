0f2a05f2b05a85ee2732e236206791a3fb364b6c
poonam <poonamsublania396@gmail.com>
2026-01-12 21:02:03 +0530

    Switch GPT to Gemini service and fix 500 errors

--- dist/routes/mockClioRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.post('/log', (req, res) => {
-    console.log('Logged billable:', req.body);
-    res.json({ success: true });
-});
-exports.default = router;
